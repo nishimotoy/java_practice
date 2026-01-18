12c2515636d46aac7297df3eff79ad6398195037
nishimotoy <czy00347@nifty.com>
2026-01-18 18:59:03 +0900

    Java practice: for

--- Main.java
+++ Main.java
@@ -1,27 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        int score = 75;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
 
-        if (score >= 80) {
-            System.out.println("合格（優）");
-        } else if (score >= 60) {
-            System.out.println("合格");
-        } else {
-            System.out.println("不合格");
+        for (int i = 0; i < 5; i++) {
+             if (i % 2 == 0) {
+                System.out.println(i + " は偶数");
+             }
         }
 
     }
 }
 
+
 /*
-■ Java 日本語が通らないとき
-原因:
-  javac の既定文字コードが Windows-31J
-
-対処:
-  javac -encoding UTF-8 Main.java
-
-よく使う:
-  javac -encoding UTF-8 Main.java && java Main
+PowerShellのコンソールで日本語の表示が化けるので、英語表記にした
 */
