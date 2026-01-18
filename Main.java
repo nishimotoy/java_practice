153eebd80cda50e39edf629993cd866f91977e10
nishimotoy <czy00347@nifty.com>
2026-01-18 18:55:13 +0900

    Initial commit: Java practice

--- Main.java
+++ Main.java
@@ -1,16 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        int a = 10;
-        int b = 3;
+        int score = 75;
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(a + b);
-
-	int c = a * b;
-	System.out.println(c);
-
+        if (score >= 80) {
+            System.out.println("合格（優）");
+        } else if (score >= 60) {
+            System.out.println("合格");
+        } else {
+            System.out.println("不合格");
+        }
 
     }
 }
+
+/*
+■ Java 日本語が通らないとき
+原因:
+  javac の既定文字コードが Windows-31J
+
+対処:
+  javac -encoding UTF-8 Main.java
+
+よく使う:
+  javac -encoding UTF-8 Main.java && java Main
+*/
