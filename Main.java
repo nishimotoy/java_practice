bfa78bca998beb33b8c0d33770cad9a5964403b7
nishimotoy <czy00347@nifty.com>
2026-01-18 18:41:22 +0900

    Add .gitignore to ignore class files

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a + b);
+
+	int c = a * b;
+	System.out.println(c);
+
+
+    }
+}
