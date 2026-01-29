9c0dd96bc2263f4b570ff8e682a0beebddc18f4d
nishimotoy <czy00347@nifty.com>
2026-01-29 18:21:56 +0900

    Separate exception handling from Controller

--- kakeibo/src/main/java/com/example/kakeibo/exception/GlobalExceptionHandler.java
+++ kakeibo/src/main/java/com/example/kakeibo/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.kakeibo.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,9 +9,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /* 
     @ExceptionHandler(ExpenseNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleExpenseNotFound(ExpenseNotFoundException e) {
         return e.getMessage();
     }
+    */
+   
+    @ExceptionHandler(ExpenseNotFoundException.class)
+    public ResponseEntity<String> handleExpenseNotFound(ExpenseNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
 }
+
+
